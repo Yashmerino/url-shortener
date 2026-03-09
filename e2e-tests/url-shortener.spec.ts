036cfc651d1e90bcc5e21599f074358ce9d372d6
Artiom Bozieac <artiomytprofile@gmail.com>
2026-03-09 17:19:52 +0200

    #11 Add e2e tests using Playwright

---
+++ e2e-tests/url-shortener.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test';
+
+const BASE_URL = 'http://localhost';
+
+test.describe('URL Shortener E2E Tests', () => {
+
+  test('create link and redirect', async ({ page }) => {
+    const originalUrl = 'https://www.google.com';
+    await page.goto(BASE_URL);
+
+    await page.fill('input[name="originalUrl"]', originalUrl);
+    await page.click('button[type="submit"]');
+
+    const resultLink = page.locator('#shortUrlResult'); 
+    await expect(resultLink).toBeVisible();
+    
+    const shortUrl = await resultLink.inputValue();
+    await page.goto(shortUrl!);
+
+    await expect(page).toHaveURL(/google.com/);
+  });
+
+  test('should redirect to 404 error page for invalid URL', async ({ page }) => {
+    await page.goto(BASE_URL + '/invalid-short-link');
+
+    await expect(page).toHaveURL(BASE_URL + '/invalid-short-link');
+    const notFoundHeader = page.locator('h1');
+    await expect(notFoundHeader).toContainText('404');
+  });
+
+  test('should work even if the link is accessed multiple times', async ({ page }) => {
+    const originalUrl = 'https://www.wikipedia.org';
+    
+    await page.goto(BASE_URL);
+    await page.fill('input[name="originalUrl"]', originalUrl);
+    await page.click('button[type="submit"]');
+    const shortUrl = await page.locator('#shortUrlResult').inputValue();
+
+    for (let i = 0; i < 3; i++) {
+        await page.goto(shortUrl!);
+        await expect(page).toHaveURL(/wikipedia.org/);
+    }
+  });
+});
